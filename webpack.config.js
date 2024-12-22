213fe5af45e43bea245b304c76d325ad40497fa0
Troy Chaplin <troy.chaplin@gmail.com>
2024-12-22 18:55:16 -0500

    add: husky

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
-const path = require('path');
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const path = require( 'path' );
 
 module.exports = {
 	...defaultConfig,
 	entry: {
 		...defaultConfig.entry(),
 		'idc-block-editor': [
-			path.resolve(__dirname, 'src/scripts/idc-block-editor.js'),
+			path.resolve( __dirname, 'src/scripts/idc-block-editor.js' ),
 		],
 		'idc-block-admin': [
-			path.resolve(__dirname, 'src/scripts/idc-block-admin.js'),
+			path.resolve( __dirname, 'src/scripts/idc-block-admin.js' ),
 		],
 		'idc-block-frontend': [
-			path.resolve(__dirname, 'src/scripts/idc-block-frontend.js'),
+			path.resolve( __dirname, 'src/scripts/idc-block-frontend.js' ),
 		],
 	},
 };
