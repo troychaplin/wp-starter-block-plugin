5a80dc6696bd16a0f3174ca2910373a0bb2049bc
Troy Chaplin <troy.chaplin@gmail.com>
2024-12-22 21:08:14 -0500

    change: php namespace

---
+++ Functions/enqueue-scripts.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace IDC\Blocks;
+
+if (! defined('ABSPATH')) {
+  exit; // Exit if accessed directly.
+}
+
+class EnqueueScripts
+{
+  public function __construct()
+  {
+    add_action('enqueue_block_editor_assets', [$this, 'enqueue_block_assets']);
+    add_action('admin_enqueue_scripts', [$this, 'enqueue_admin_assets']);
+    add_action('wp_enqueue_scripts', [$this, 'enqueue_frontend_assets']);
+  }
+
+  /**
+   * Enqueues the block editor JavaScript assets for the IDC Block Plugin.
+   *
+   * This function registers and enqueues the JavaScript file required for the block editor.
+   * The script is dependent on several WordPress packages such as wp-blocks, wp-components, wp-data, etc.
+   *
+   * @return void
+   */
+  public function enqueue_block_assets()
+  {
+    wp_enqueue_script(
+      'idc-block-editor-js',
+      plugin_dir_url(__FILE__) . '../build/idc-block-editor.js',
+      array('wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'),
+      null,
+      false
+    );
+  }
+
+  /**
+   * Enqueues the admin JavaScript assets for the IDC Block Plugin.
+   *
+   * This function registers and enqueues the JavaScript file used in the admin
+   * area of the IDC Block Plugin. The script is loaded from the build directory
+   * and is set to load in the footer of the admin page.
+   *
+   * @return void
+   */
+  public function enqueue_admin_assets()
+  {
+    wp_enqueue_script(
+      'idc-block-admin-js',
+      plugin_dir_url(__FILE__) . '../build/idc-block-admin.js',
+      array(),
+      null,
+      true
+    );
+  }
+
+  /**
+   * Enqueues the frontend JavaScript assets for the IDC Block plugin.
+   *
+   * This function registers and enqueues the JavaScript file located at
+   * '../build/idc-block-frontend.js' for use on the frontend of the site.
+   *
+   * @return void
+   */
+  public function enqueue_frontend_assets()
+  {
+    wp_enqueue_script(
+      'idc-block-frontend-js',
+      plugin_dir_url(__FILE__) . '../build/idc-block-frontend.js',
+      array(),
+      null,
+      true
+    );
+  }
+}
---
+++ Functions/register-blocks.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace IDC\Blocks;
+
+if (! defined('ABSPATH')) {
+  exit; // Exit if accessed directly.
+}
+
+class RegisterBlocks
+{
+  public function __construct()
+  {
+    add_action('init', [$this, 'register_blocks']);
+  }
+
+  /**
+   * Registers custom Gutenberg blocks.
+   *
+   * This method registers blocks by first checking if the function 
+   * `wp_register_block_metadata_collection` exists. If it does, it registers 
+   * the block metadata collection from the specified directory and manifest file.
+   * 
+   * Then, it retrieves all block directories from the build path and registers 
+   * each block type found within those directories.
+   *
+   * @return void
+   */
+  public function register_blocks()
+  {
+    if (function_exists('wp_register_block_metadata_collection')) {
+      wp_register_block_metadata_collection(
+        __DIR__ . '/../build',
+        __DIR__ . '/../build/blocks-manifest.php'
+      );
+    }
+
+    $blocks = glob(__DIR__  . '/../build/blocks/*', GLOB_ONLYDIR);
+
+    foreach ($blocks as $block) {
+      register_block_type($block);
+    }
+  }
+}
