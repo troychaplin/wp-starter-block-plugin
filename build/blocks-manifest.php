dd1c357bbf8e8f26791dea6963a3ae5f84f60286
Troy Chaplin <troy.chaplin@gmail.com>
2024-12-30 20:22:32 -0500

    change: build

---
+++ build/blocks-manifest.php
@@ -0,0 +1,40 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'dynamic-block' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'idc-block/dynamic-block',
+		'version' => '0.1.0',
+		'title' => 'Dynamic Block',
+		'category' => 'listings',
+		'icon' => 'smiley',
+		'description' => 'Example block scaffolded with Create Block tool.',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'render' => 'file:./render.php',
+		'textdomain' => 'wp-starter-blocks'
+	),
+	'static-block' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'idc-block/static-block',
+		'version' => '0.1.0',
+		'title' => 'Static Block',
+		'category' => 'listings',
+		'icon' => 'smiley',
+		'description' => 'Example block scaffolded with Create Block tool.',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'render' => 'file:./render.php',
+		'textdomain' => 'wp-starter-blocks'
+	)
+);
---
+++ build/idc-block-editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5e24ef79be3ae5c1ce12');
