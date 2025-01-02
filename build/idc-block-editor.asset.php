695e745ea643321b9bd14d4ef1740bbac7f4a607
Troy Chaplin <troy.chaplin@gmail.com>
2025-01-02 00:13:12 -0500

    finish: block options

--- build/idc-block-editor.asset.php
+++ build/idc-block-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'a5e2d810cb6c7ec10df8');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '4270d5d32f730767c4d4');
--- src/blocks/lead-in/edit.js
+++ src/blocks/lead-in/edit.js
@@ -1,7 +1,8 @@
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, PlainText } from '@wordpress/block-editor';
 import { LeadIn, Section } from '@troychaplin79/idc-frontend-ui';
 
-export default function Edit() {
+export default function Edit( { attributes, setAttributes } ) {
+	const { topText, bottomText, asideText } = attributes;
 	return (
 		<div
 			{ ...useBlockProps( {
@@ -10,9 +11,57 @@ export default function Edit() {
 		>
 			<Section>
 				<LeadIn
-					headerBottom="As simple as A-B-C"
-					headerTop="Our Process"
-					text="We take the anxiety and uncertainty out of the document apostille process in these three simple steps!"
+					headerTop={
+						<PlainText
+							placeholder="Our Process"
+							className="block text-base italic not-prose md:text-lg text-idc-black-500"
+							style={ {
+								border: 'none',
+								padding: '0',
+								margin: '0',
+							} }
+							value={ topText }
+							onChange={ ( value ) =>
+								setAttributes( { topText: value } )
+							}
+							allowedFormats={ [] }
+							disableLineBreaks={ true }
+						/>
+					}
+					headerBottom={
+						<PlainText
+							placeholder="As simple as A-B-C"
+							className="block text-2xl italic font-medium not-prose md:text-3xl text-idc-orange-600"
+							style={ {
+								border: 'none',
+								padding: '0',
+								margin: '0',
+							} }
+							value={ bottomText }
+							onChange={ ( value ) =>
+								setAttributes( { bottomText: value } )
+							}
+							allowedFormats={ [] }
+							disableLineBreaks={ true }
+						/>
+					}
+					text={
+						<PlainText
+							placeholder="We take the anxiety and uncertainty out of the document apostille process in these three simple steps!"
+							className="text-base md:text-lg"
+							style={ {
+								border: 'none',
+								padding: '0',
+								margin: '0',
+							} }
+							value={ asideText }
+							onChange={ ( value ) =>
+								setAttributes( { asideText: value } )
+							}
+							allowedFormats={ [] }
+							disableLineBreaks={ true }
+						/>
+					}
 				/>
 			</Section>
 		</div>
