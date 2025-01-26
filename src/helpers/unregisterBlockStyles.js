1b5e7d090b8e6875903df7bbbccb0be7da4f5e02
Troy Chaplin <troy.chaplin@gmail.com>
2025-01-26 16:14:20 -0500

    change: block styles

--- src/helpers/unregisterBlockStyles.js
+++ src/helpers/unregisterBlockStyles.js
@@ -1,11 +1,26 @@
 import { unregisterBlockStyle } from '@wordpress/blocks';
+import { addFilter } from '@wordpress/hooks';
 import domReady from '@wordpress/dom-ready';
 
 domReady( () => {
-	// Unregister block styles
 	unregisterBlockStyle( 'core/button', [ 'outline' ] );
 	unregisterBlockStyle( 'core/gallery', [ 'default', 'rounded' ] );
 	unregisterBlockStyle( 'core/image', [ 'default', 'rounded' ] );
 	unregisterBlockStyle( 'core/quote', [ 'plain' ] );
 	unregisterBlockStyle( 'core/table', [ 'regular', 'stripes' ] );
 } );
+
+function modifyCoreButton( settings, name ) {
+	if ( [ 'core/buttons', 'core/button' ].includes( name ) ) {
+		settings.styles = settings.styles.map( ( style ) =>
+			style.name === 'fill' ? { ...style, label: 'Dark Blue' } : style
+		);
+	}
+
+	return settings;
+}
+addFilter(
+	'blocks.registerBlockType',
+	'cutheme/core-button-modifications',
+	modifyCoreButton
+);
